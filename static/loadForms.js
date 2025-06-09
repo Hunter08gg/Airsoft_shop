5da1be89eaec2557c4232e6873afd29d554dd6d7
Hunter08gg <alex.mdn5000@gmail.com>
2025-06-09 19:45:48 +0300

    Добавлены стили, классы, базовая и каталог страницы

---
+++ static/loadForms.js
@@ -0,0 +1,13 @@
+async function loadCreaters() {
+    const res = await fetch("/products/all-creaters/")
+    const data = await res.json()
+    data.forEach(creater => {
+        const option = document.createElement("option")
+        option.textContent = creater.name
+        option.setAttribute("value", creater.id)
+        const selectEl = document.querySelector("#creater")
+        selectEl.appendChild(option)
+    })
+}
+
+loadCreaters()
