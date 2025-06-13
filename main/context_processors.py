bbe7f1811c9f1010ec4e3b54b0988c87020ef12a
Hunter08gg <alex.mdn5000@gmail.com>
2025-06-13 21:02:28 +0300

    Вошёл с домашнего компьютера и внес изменения, недоработан

--- main/context_processors.py
+++ main/context_processors.py
@@ -1,4 +1,4 @@
-def cart_context(request):
+def creators_context(request):
     cart_count = 0
     if hasattr(request, 'session') and 'cart_count' in request.session:
         cart_count = request.session.get('cart_count', 0)
