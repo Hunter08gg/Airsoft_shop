85973cc4aece6f8674c2cd04640b8e762920f460
Hunter08gg <alex.mdn5000@gmail.com>
2025-06-25 19:26:45 +0300

    Изменена страница <О нас>

--- airsort_shop/settings.py
+++ airsort_shop/settings.py
@@ -119,8 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR/"static"]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #AUTH_USER_MODEL = 'accounts.User'
 LOGIN_REDIRECT_URL = 'index'
